36a87f743a57b37e673be2ce6bf8ceda84059a71
Arman-Aghajanyan <119808906+Arman-Aghajanyan@users.noreply.github.com>
2023-09-24 20:42:30 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Board from "./assets/Board";
+
+function App(props) {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
